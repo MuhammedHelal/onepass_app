d0f2ddb647ff5b7c03df14a71f6b0c9b5b0c2c49
Mohamed Ashraf <mohamed8ashraf2@gmail.com>
2025-03-27 15:06:52 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:onepass/onepass_app.dart';
+
+void main() {
+  runApp(const OnePassApp());
+}
---
+++ lib/onepass_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OnePassApp extends StatelessWidget {
+  const OnePassApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Placeholder(),
+    );
+  }
+}
